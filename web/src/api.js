34a393b31f1c77eae79bd8703fae697a6f9946bb
ishi1301 <ishhhhhita@gmail.com>
2026-02-19 14:21:02 +0530

    update API to use VITE

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//     baseURL: "https://marketplace-backend-ezd4.onrender.com"
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://marketplace-backend-ezd4.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default API;
